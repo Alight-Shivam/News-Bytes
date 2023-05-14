0ca371ff0e153217c5658d608aefecd84614fe86
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-05-14 15:48:26 +0530

    News Bytes

---
+++ src/components/News.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Newsitems from './Newsitems'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        Hey!
+        <Newsitems/>
+        <Newsitems/>
+        <Newsitems/>
+        <Newsitems/>
+        <Newsitems/>
+        <Newsitems/>
+      </div>
+    )
+  }
+}
+
+export default News
